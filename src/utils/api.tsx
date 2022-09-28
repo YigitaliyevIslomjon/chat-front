24d67278dea2c365f7539374f437d65124af2642
YigitaliyevIslomjon <77922591+YigitaliyevIslomjon@users.noreply.github.com>
2022-09-28 20:41:30 +0500

    changes

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://app-like-gmail.herokuapp.coms/api/",
+  baseURL: "https://app-like-gmail.herokuapp.com/api/",
 });
 
 // Add a request interceptor
